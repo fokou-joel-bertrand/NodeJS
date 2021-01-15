a39fb16202bf10a805a86305739ec846299a6b1a
fokou-joel-bertrand <joelfokou99@gmail.com>
2021-01-15 12:46:34 +0100

    mongoose

---
+++ node-mongoose/index.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes.js');
+
+const url = "mongodb://localhost:27017/conFusion";
+const connect = mongoose.connect(url);
+
+
+connect.then((db) => {
+    console.log("Connected correctly to server");
+
+    Dishes.create({
+        name: 'Uthappizza',
+        description: 'test'
+    })
+       .then(dish => {
+           console.log(dish)
+
+          return Dishes.find({}).exec();
+       })
+       .then(dishes => {
+           console.log(dishes);
+
+           return Dishes.deleteMany({});
+       })
+       .then(() => {
+           return mongoose.connection.close();
+       })
+       .catch((err) => {
+           console.log(err);
+       });
+});
+
+
+/*connect.then((db) => {
+    console.log("Connected correctly to server");
+
+    const newDish = Dishes({
+        name: 'Uthappizza',
+        description: 'test'
+    });
+    
+
+    newDish.save()
+       .then(dish => {
+           console.log(dish)
+
+          return Dishes.find({}).exec();
+       })
+       .then(dishes => {
+           console.log(dishes);
+
+           return Dishes.deleteMany({});
+       })
+       .then(() => {
+           return mongoose.connection.close();
+       })
+       .catch((err) => {
+           console.log(err);
+       });
+});*/
+
